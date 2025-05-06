9f7db89b24201436a89df2318ec747a7300ccca3
Jaromek - Gliwice <jakub.jaromin2004@gmail.com>
2025-05-06 19:55:32 +0200

    changes

---
+++ features/chroma_features.py
@@ -0,0 +1,5 @@
+import numpy as np
+class ChromaFeatureExtractor:
+    def __init__(self, sr=22050, n_chroma=12):
+        self.sr = sr
+        self.n_chroma = n_chroma
--- signalProcessing/pitchDetection.py
+++ signalProcessing/pitchDetection.py
@@ -1,5 +1,5 @@
 import numpy as np
-from signal_processing.fft import FFTProcessor
+from signalProcessing.fastFourierTransform import FFTProcessor
 
 NOTE_NAMES = [
     "C", "C#", "D", "D#", "E", "F",
